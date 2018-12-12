767cffc7a3a165f169346c254403f075ea70f436
wennshenglim <wennshenglim@DESKTOP-NHSV9VG>
2018-12-12 20:01:03 +0800

    CustomerMaintenance_SearchViewDelete v3.5

--- src/CustomerMaintenance.java
+++ src/CustomerMaintenance.java
@@ -4,22 +4,35 @@
  * @author wennshenglim
  */
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class CustomerMaintenance {
 
     static Scanner s = new Scanner(System.in);
-    static List<Customers> ConsumerList = new ArrayList<Customers>();
-    static List<Customers> CorporateCustList = new ArrayList<Customers>();
+    static List<Consumer> ConsumerList = new ArrayList<Consumer>();
+    static List<CorporateCust> CorporateCustList = new ArrayList<CorporateCust>();
     static List<Staff> StaffList = new ArrayList<Staff>();
     static Staff staffLoggedIn;
 
+    public static List<Consumer> getConsumerList() {
+        return ConsumerList;
+    }
+
+    public static List<CorporateCust> getCorporateCustList() {
+        return CorporateCustList;
+    }
+
     public static void main(String[] args) {
-        Customers consumer = new Customers("wennshenglim", "LimWS", "wslim1998@gmail.com", "0123456789", "13A-12 PV16");
-        ConsumerList.add(consumer);
-        Customers corporateCust = new Customers("evelnicky", "Evel", "evel@gmail.com", "0164118558", "TARUC", "Jalan Genting Klang", 250);
-        CorporateCustList.add(corporateCust);
+        Consumer consumer1 = new Consumer("wennshenglim", "LimWS", "wslim1998@gmail.com", "0123456789", "13A-12 PV16");
+        Consumer consumer2 = new Consumer("waichong", "Ng Wai Chong", "waichong98@gmail.com", "0175847895", "Sungai Batu");
+        ConsumerList.add(consumer1);
+        ConsumerList.add(consumer2);
+        CorporateCust corporateCust1 = new CorporateCust("evelnicky", "Evel", "evel@gmail.com", "0164118558", "TARUC", "Jalan Genting Klang", 250);
+        CorporateCust corporateCust2 = new CorporateCust("khlim666", "Lim Kai Heng", "khlim666@gmail.com", "01115422527", "KDU", "Jalan Anson", 250);
+        CorporateCustList.add(corporateCust1);
+        CorporateCustList.add(corporateCust2);
         Staff staff = new Staff("admin", "FlowerShop", "admin", "0134789658");
         StaffList.add(staff);
         LogInMenu();
@@ -41,10 +54,11 @@ public class CustomerMaintenance {
                             && StaffList.get(i).getPassword().equals(password)) {
                         System.out.println("\nWelcome back, " + username + "!!");
                         check = true;
+                        staffLoggedIn = new Staff(username, StaffList.get(i).getName(), StaffList.get(i).getPassword(), StaffList.get(i).getPhoneNo());
                         CustomerMaintenanceMenu();
                         flush();
                     } else {
-                        System.out.print("\nInvalid login credentials!! Please reenter");
+                        System.out.print("\nInvalid login credentials!! Please reenter\n");
                         check = false;
                     }
                 }
@@ -159,7 +173,7 @@ public class CustomerMaintenance {
         String phoneNo = s.nextLine();
         System.out.print("Home Address: ");
         String address = s.nextLine();
-        Customers consumer = new Customers(username, name, email, phoneNo, address);
+        Consumer consumer = new Consumer(username, name, email, phoneNo, address);
         ConsumerList.add(consumer);
         System.out.println("\nNew account created successfully!\n");
         CustomerMaintenanceMenu();
@@ -198,7 +212,7 @@ public class CustomerMaintenance {
         System.out.print("Credit Limit: ");
         int creditLimit = s.nextInt();
 
-        Customers corporateCust = new Customers(username, name, email, phoneNo, corporateName, address, creditLimit);
+        CorporateCust corporateCust = new CorporateCust(username, name, email, phoneNo, corporateName, address, creditLimit);
         CorporateCustList.add(corporateCust);
         System.out.println("\nNew account created successfully!\n");
         CustomerMaintenanceMenu();
@@ -282,4 +296,112 @@ public class CustomerMaintenance {
         }
     }
 
+    public static void DeleteAcc() {
+        System.out.println("");
+        boolean found = false;
+        System.out.println("\nDelete Account\n===============");
+        System.out.println("Please enter username!");
+        System.out.print("Username: ");
+        String username = s.nextLine();
+
+        for (int i = 0; i < ConsumerList.size(); i++) {
+            if (ConsumerList.get(i).getUsername().equals(username)) {
+                System.out.println("\nUsername found!!");
+                System.out.println("Username: " + username);
+                System.out.println("Name: " + ConsumerList.get(i).getName());
+                System.out.println("Email Address: " + ConsumerList.get(i).getEmail());
+                System.out.println("Phone Number: " + ConsumerList.get(i).getPhoneNo());
+                System.out.println("Home Address: " + ConsumerList.get(i).getAddress());
+                found = true;
+                if (found == true) {
+                    System.out.print("Are you sure you want to delete this account?(Y/N): ");
+                    char reply = s.next().charAt(0);
+                    if (reply == 'Y' || reply == 'y') {
+                        flush();
+                        ConsumerList.remove(i);
+                        System.out.println("\nAccount is deleted successfully!!");
+                        System.out.println("Redirect to main menu...");
+                        CustomerMaintenanceMenu();
+
+                    } else {
+                        flush();
+                        CustomerMaintenanceMenu();
+                        flush();
+                    }
+                }
+            }
+        }
+        if (!found) {
+            for (int i = 0; i < CorporateCustList.size(); i++) {
+                if (CorporateCustList.get(i).getUsername().equals(username)) {
+                    System.out.println("\nUsername found!!");
+                    System.out.println("Username: " + username);
+                    System.out.println("Name: " + CorporateCustList.get(i).getName());
+                    System.out.println("Email Address: " + CorporateCustList.get(i).getEmail());
+                    System.out.println("Phone Number: " + CorporateCustList.get(i).getPhoneNo());
+                    System.out.println("Corporate Name: " + CorporateCustList.get(i).getCorporateName());
+                    System.out.println("Corporate Address: " + CorporateCustList.get(i).getAddress());
+                    System.out.println("Credit Limit: " + CorporateCustList.get(i).getCreditLimit());
+                    found = true;
+                    if (found == true) {
+                        System.out.print("Are you sure you want to delete this account?(Y/N): ");
+                        char reply = s.next().charAt(0);
+                        if (reply == 'Y' || reply == 'y') {
+                            flush();
+                            CorporateCustList.remove(i);
+                            System.out.println("\nAccount is deleted successfully!!");
+                            System.out.println("Redirect to main menu...");
+                            CustomerMaintenanceMenu();
+
+                        } else {
+                            flush();
+                            CustomerMaintenanceMenu();
+                            flush();
+                        }
+                    }
+                }
+            }
+            System.out.print("\nNo search found! Search again to delete?(Y?N): ");
+            char reply = s.next().charAt(0);
+            if (reply == 'Y' || reply == 'y') {
+                flush();
+                DeleteAcc();
+                flush();
+            } else {
+                flush();
+                CustomerMaintenanceMenu();
+                flush();
+            }
+        }
+    }
+
+    public static void ViewAllAcc() {
+        System.out.println("\nView All Account\n================");
+
+        System.out.println("Consumer Account List");
+        int indexConsumer = 0;
+        int index = 0;
+        System.out.println("+-----+------------------+----------------------+--------------------------+----------------+------------------------------+");
+        System.out.format("| %s | %-16s | %-20s | %-24s | %-14s | %-28s |\n", "No.", "Username", "Name", "Email", "Contact Number", "Home Address");
+        System.out.println("+-----+------------------+----------------------+--------------------------+----------------+------------------------------+");
+        for (Consumer consumer : getConsumerList()) {
+            ++index;
+            System.out.format("| %-3d | %s\n", ++indexConsumer, consumer);
+        }
+        System.out.println("+-----+------------------+----------------------+--------------------------+----------------+------------------------------+");
+        int indexCorporateCust = 0;
+        System.out.println("\nCorporate Customer Account List");
+        System.out.println("+-----+------------------+----------------------+--------------------------+----------------+----------------------+------------------------------+--------------+");
+        System.out.format("| %s | %-16s | %-20s | %-24s | %-14s | %-20s | %-28s | %-12s |\n", "No.", "Username", "Name", "Email", "Contact Number", "Corporate Name", "Corporate Address", "Credit Limit");
+        System.out.println("+-----+------------------+----------------------+--------------------------+----------------+----------------------+------------------------------+--------------+");
+        for (CorporateCust corporateCust : getCorporateCustList()) {
+            ++index;
+            System.out.format("| %-3d | %s\n", ++indexCorporateCust, corporateCust);
+        }
+        System.out.println("+-----+------------------+----------------------+--------------------------+----------------+----------------------+------------------------------+--------------+");
+        System.out.println("Total customer accounts: " + index);
+        flush();
+        System.out.println("Redirect to main menu...");
+        CustomerMaintenanceMenu();
+    }
 }
